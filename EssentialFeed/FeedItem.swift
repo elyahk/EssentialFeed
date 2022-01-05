2ef937e69578a145edd45ccf372253a84f8cdb41
Eldorbek <eldorbek.nusratov.98@gmail.com>
2022-01-05 22:35:13 +0500

    Create the FeedItem

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Eldorbek on 05/01/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let locaation: String?
+    let imageUrl: URL
+}
