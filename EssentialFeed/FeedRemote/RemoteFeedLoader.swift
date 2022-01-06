3bd6ee94c22d142fa164aed60139ddc4103322ae
Eldorbek <eldorbek.nusratov.98@gmail.com>
2022-01-06 21:41:47 +0500

    Delivers connectivity error on client error

--- EssentialFeed/FeedRemote/RemoteFeedLoader.swift
+++ EssentialFeed/FeedRemote/RemoteFeedLoader.swift
@@ -8,19 +8,23 @@
 import Foundation
 
 public protocol HTTPClient {
-    func get(from url: URL)
+    func get(from url: URL, completion: @escaping (Error) -> Void)
 }
 
 final public class RemoteFeedLoader {
     private var url: URL
     private var client: HTTPClient
 
+    public enum Error {
+        case connectivity
+    }
+
     public init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
     }
 
-    public func load() {
-       client.get(from: url)
+    public func load(completion: @escaping (Error) -> Void = { _ in }) {
+        client.get(from: url) { error in completion(.connectivity)}
     }
 }
--- EssentialFeedTests/FeedFeatureLoaderTests.swift
+++ EssentialFeedTests/FeedFeatureLoaderTests.swift
@@ -34,6 +34,20 @@ class FeedFeatureLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedUrls, [url, url])
     }
 
+    func test_load_deliversErrorOnClientError() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+
+        client.error = NSError(domain: "Test", code: 0)
+
+        var capturedError: RemoteFeedLoader.Error?
+        sut.load { error in
+            capturedError = error
+        }
+
+        XCTAssertEqual(capturedError, .connectivity)
+    }
+
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (RemoteFeedLoader, HTTPClientSpy) {
@@ -44,11 +58,13 @@ class FeedFeatureLoaderTests: XCTestCase {
 
     private class HTTPClientSpy: HTTPClient {
         var requestedUrls = [URL]()
+        var error: Error?
 
-        func get(from url: URL) {
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
             requestedUrls.append(url)
+            if let error = error {
+                completion(error)
+            }
         }
-
     }
-
 }
