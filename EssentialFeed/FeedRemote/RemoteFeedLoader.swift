57469c4c31c13c4dfbc3b1ca29187f49591396a5
Eldorbek <eldorbek.nusratov.98@gmail.com>
2022-01-06 21:57:40 +0500

    Remove default value in production to test target

--- EssentialFeed/FeedRemote/RemoteFeedLoader.swift
+++ EssentialFeed/FeedRemote/RemoteFeedLoader.swift
@@ -24,7 +24,7 @@ final public class RemoteFeedLoader {
         self.client = client
     }
 
-    public func load(completion: @escaping (Error) -> Void = { _ in }) {
+    public func load(completion: @escaping (Error) -> Void) {
         client.get(from: url) { error in completion(.connectivity)}
     }
 }
