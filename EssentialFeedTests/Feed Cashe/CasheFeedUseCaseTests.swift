b2f89c93df834188d0e2209d118052a2a99947ce
Eldorbek <eldorbek.nusratov.98@gmail.com>
2022-02-24 08:30:34 +0500

    Delete cashed feed items upon saving

--- EssentialFeedTests/Feed Cashe/CasheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cashe/CasheFeedUseCaseTests.swift
@@ -6,9 +6,14 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class FeedStore {
     var deleteCashedFeedCallCount: Int = 0
+
+    func deleteCashedFeed() {
+        deleteCashedFeedCallCount += 1
+    }
 }
 
 class LocalFeedLoader {
@@ -17,13 +22,37 @@ class LocalFeedLoader {
     init(feedStore: FeedStore) {
         self.feedStore = feedStore
     }
+
+    func save(_ items: [FeedItem]) {
+        feedStore.deleteCashedFeed()
+    }
 }
 
 class CasheFeedUseCaseTests: XCTestCase {
-    func test_() {
+    func test_init_doesNotDeleteCasheUponCreation() {
         let feedStore = FeedStore()
         _ = LocalFeedLoader(feedStore: feedStore)
 
         XCTAssertEqual(feedStore.deleteCashedFeedCallCount, 0)
+    }
+
+    func test_save_deleteCashe() {
+        let feedStore = FeedStore()
+        let sut = LocalFeedLoader(feedStore: feedStore)
+        let items = [uniqueItem(), uniqueItem()]
+
+        sut.save(items)
+
+        XCTAssertEqual(feedStore.deleteCashedFeedCallCount, 1)
+    }
+
+    // MARK: - Helpers
+
+    private func uniqueItem() -> FeedItem {
+        FeedItem(id: UUID(), description: "any", location: "any", imageUrl: anyURL())
+    }
+
+    private func anyURL() -> URL {
+        return URL(string: "https://a-url.com")!
     }
 }
