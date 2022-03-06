7e027bc9535c92baa36b9dfaf11067c92e4bb4d5
Eldorbek <eldorbek.nusratov.98@gmail.com>
2022-03-06 22:34:24 +0500

    Load command delivers delete on more than seven days timestamp

--- EssentialFeedTests/Feed Cashe/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cashe/LoadFeedFromCacheUseCaseTests.swift
@@ -118,6 +118,18 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.recievedMessages, [.retrieve, .deleteCachedFeed])
     }
 
+    func test_load_deleteCacheOnMoreThanSevenDaysOld() {
+        let currentDate = Date()
+        let (store, sut) = makeSUT(currentDate: { currentDate })
+
+        sut.load() { _ in }
+
+        let lessThanSevenDays = currentDate.adding(days: -7).adding(days: -1)
+        store.completeRetrieval(with: uniqueImageFeed().locals, timestamp: lessThanSevenDays)
+
+        XCTAssertEqual(store.recievedMessages, [.retrieve, .deleteCachedFeed])
+    }
+
     // MARK: - Helper
 
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (store: FeedStoreSpy, sut: LocalFeedLoader) {
