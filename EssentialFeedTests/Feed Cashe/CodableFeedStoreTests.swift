ae7965248a50aff2b7fe073b0d54ce1b0042f181
Eldorbek <eldorbek.nusratov.98@gmail.com>
2022-03-08 22:22:57 +0500

    Extract reusable `FeedStoreSpecs` helper method into a shared scope so it can be used by other `FeedStore` implementation tests

--- EssentialFeedTests/Feed Cashe/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cashe/CodableFeedStoreTests.swift
@@ -184,59 +184,6 @@ class CodableFeedStoreTests: XCTestCase, FailableFeedStore {
         return sut
     }
 
-    @discardableResult
-    private func deleteCache(from sut: FeedStore) -> Error? {
-        let exp = expectation(description: "Wait for cache deletion")
-        var recievedError: Error?
-        sut.deleteCashedFeed { deletionError in
-            recievedError = deletionError
-            exp.fulfill()
-
-        }
-        wait(for: [exp], timeout: 1.0)
-
-        return recievedError
-    }
-
-    @discardableResult
-    private func insert(feed: [LocalFeedImage], timestamp: Date, to sut: FeedStore) -> Error? {
-        let exp = expectation(description: "Wait for cache retrieval")
-        var recievedError: Error?
-        sut.insert(feed, timestamp: timestamp) { insertionError in
-            recievedError = insertionError
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
-
-        return recievedError
-    }
-
-    private func expect(_ sut: FeedStore, toRetrieveTwice expectedResult: RetrievalResult) {
-        expect(sut, toRetrieve: expectedResult)
-        expect(sut, toRetrieve: expectedResult)
-    }
-
-    private func expect(_ sut: FeedStore, toRetrieve expectedResult: RetrievalResult) {
-        let exp = expectation(description: "Wait for retrieve")
-        sut.retrieve { recievedResult in
-            switch (recievedResult, expectedResult) {
-            case (.empty, .empty), (.failure, .failure):
-                break
-
-            case let (.found(recievedFeed, recievedTimestamp), .found(expectedFeed, expectedTimestamp)):
-                XCTAssertEqual(recievedFeed, expectedFeed)
-                XCTAssertEqual(recievedTimestamp, expectedTimestamp)
-
-            default:
-                XCTFail("Expected to retrieve \(expectedResult), got \(recievedResult) instead")
-            }
-
-            exp.fulfill()
-        }
-
-        wait(for: [exp], timeout: 1.0)
-    }
-
     private func setUpEmptyState() {
         deleteStoreSideEffects()
     }
