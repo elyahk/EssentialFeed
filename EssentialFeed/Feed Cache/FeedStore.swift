8d61057a41536a218c19be5a5fba6546c4e23b63
Eldorbek <eldorbek.nusratov.98@gmail.com>
2022-03-08 20:24:32 +0500

    Add documentation  to clients to be responsible for completion handler in any thread

--- EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -18,8 +18,16 @@ public protocol FeedStore {
     typealias InsertionCompletion = (Error?) -> Void
     typealias RetrievalCompletion = (RetrievalResult) -> Void
 
+    /// The completion handler can be invoked in any thread
+    /// Clients are responsible to dispatch to approporiate threads, if needed
     func deleteCashedFeed(completion: @escaping DeletionCompletion)
+
+    /// The completion handler can be invoked in any thread
+    /// Clients are responsible to dispatch to approporiate threads, if needed
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
+
+    /// The completion handler can be invoked in any thread
+    /// Clients are responsible to dispatch to approporiate threads, if needed
     func retrieve(completion: @escaping RetrievalCompletion)
 }
 
