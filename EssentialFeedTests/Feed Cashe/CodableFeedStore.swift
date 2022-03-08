eadd17f80b32fca38502580d358e358e8dae9259
Eldorbek <eldorbek.nusratov.98@gmail.com>
2022-03-08 11:37:29 +0500

    Move `Codable` conformance from the framework-agnostic `LocalFeedImage` type to the new framework-specific `CodableFFeedImage` type

--- EssentialFeedTests/Feed Cashe/CodableFeedStore.swift
+++ EssentialFeedTests/Feed Cashe/CodableFeedStore.swift
@@ -11,8 +11,28 @@ import EssentialFeed
 
 class CodableFeedStore {
     private struct Cache: Codable {
-        let feed: [LocalFeedImage]
+        let feed: [CodableFeedImage]
         let timestamp: Date
+
+        var feedLocal: [LocalFeedImage] {
+            feed.map { $0.local }
+        }
+    }
+
+    private class CodableFeedImage: Codable {
+        private let id: UUID
+        private let description: String?
+        private let location: String?
+        private let url: URL
+
+        internal init(_ image: LocalFeedImage) {
+            self.id = image.id
+            self.description = image.description
+            self.location = image.location
+            self.url = image.url
+        }
+
+        var local: LocalFeedImage { LocalFeedImage(id: id, description: description, location: location, url: url) }
     }
 
     private let storURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
@@ -22,12 +42,12 @@ class CodableFeedStore {
 
         let decoder = JSONDecoder()
         let cache = try! decoder.decode(Cache.self, from: data)
-        completion(.found(cache.feed, cache.timestamp))
+        completion(.found(cache.feedLocal, cache.timestamp))
     }
 
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
         let encoder = JSONEncoder()
-        let encoded = try! encoder.encode(Cache(feed: feed, timestamp: timestamp))
+        let encoded = try! encoder.encode(Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp))
         try! encoded.write(to: storURL)
         completion(nil)
     }
