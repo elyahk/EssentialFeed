c522d538322d7e93afbf6aec3e19156edf96ac37
Eldorbek <eldorbek.nusratov.98@gmail.com>
2022-03-08 19:53:04 +0500

    Proved that the `CodableFeedStore` side effect run serially

--- EssentialFeedTests/Feed Cashe/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cashe/CodableFeedStoreTests.swift
@@ -124,6 +124,34 @@ class CodableFeedStoreTests: XCTestCase {
         XCTAssertNotNil(deletionError, "Expected delete error")
     }
 
+    func test_storeSideEffects_runSerially() {
+        let sut = makeSUT()
+        var completedOperationsInOrder = [XCTestExpectation]()
+
+        let op1 = expectation(description: "Wait for insertion")
+        sut.insert([], timestamp: Date()) { _ in
+            completedOperationsInOrder.append(op1)
+            op1.fulfill()
+        }
+
+        let op2 = expectation(description: "Wait for deletion")
+        sut.deleteCashedFeed { _ in
+            completedOperationsInOrder.append(op2)
+            op2.fulfill()
+        }
+
+        let op3 = expectation(description: "Wait for retrieving")
+        sut.retrieve { _ in
+            completedOperationsInOrder.append(op3)
+            op3.fulfill()
+        }
+
+        wait(for: [op1, op2, op3], timeout: 5.0)
+
+        XCTAssertEqual(completedOperationsInOrder, [op1, op2, op3])
+    }
+
+
     // MARK: - Helpers
 
     private var testSpecificStoreURL: URL { FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathComponent("\(type(of: self)).store") }
